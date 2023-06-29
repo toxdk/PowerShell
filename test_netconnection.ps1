1f3db8b2d89e1a8fa22a1a36e3895a3720090565
Tox - Michael <35349119+toxdk@users.noreply.github.com>
2023-06-29 15:24:10 +0200

    Update test_netconnection.ps1

--- test_netconnection.ps1
+++ test_netconnection.ps1
@@ -1,11 +1,12 @@
 
 Test-NetConnection
 
-Test-NetConnection -InformationLevel "Quiet"
-Test-NetConnection -InformationLevel "Detailed"
+Test-NetConnection -InformationLevel 'Quiet'
+Test-NetConnection -InformationLevel 'Detailed'
 
-Test-NetConnection -ComputerName "127.0.0.1" -InformationLevel "Quiet"
-Test-NetConnection -ComputerName "127.0.0.1" -InformationLevel "Detailed"
+Test-NetConnection -ComputerName '127.0.0.1' -InformationLevel 'Quiet'
+Test-NetConnection -ComputerName '127.0.0.1' -InformationLevel 'Detailed'
 
-Test-NetConnection -ComputerName "127.0.0.1" -Port "80" -InformationLevel "Quiet"
-Test-NetConnection -ComputerName "127.0.0.1" -Port "80" -InformationLevel "Detailed"
+Test-NetConnection -ComputerName '127.0.0.1' -Port '80' -InformationLevel 'Quiet'
+Test-NetConnection -ComputerName '127.0.0.1' -Port '80' -InformationLevel 'Detailed'
+
